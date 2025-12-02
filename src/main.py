34f566fa62b74bf3c042a508e19f9c68aa6777d2
Hamano0813 <hamano0813@gmail.com>
2025-12-02 23:30:23 +0800

    添加主函数，整合DPI缩放设置和主窗口初始化

--- src/main.py
+++ src/main.py
@@ -1,5 +1,24 @@
+import os
+import sys
+
+from PySide6.QtWidgets import QApplication
+
+import config
+from gui.main_window import MainWindow
+
+sys.path.append(os.path.dirname(__file__))
+
+
+def scale_dpi():
+    os.environ["QT_ENABLE_HIGHDPI_SCALING"] = "0"
+    os.environ["QT_SCALE_FACTOR"] = str(config.option.get(config.option.dpi))
+
+
 def main():
-    print("Hello from srwalpha!")
+    scale_dpi()
+    app = QApplication(sys.argv)
+    MainWindow()
+    sys.exit(app.exec())
 
 
 if __name__ == "__main__":

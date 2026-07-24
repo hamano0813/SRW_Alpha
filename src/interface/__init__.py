f2c69bd9d78ee4c9ebc9e617152b6321de130528
Hamano0813 <hamano0813@gmail.com>
2026-07-24 15:06:32 +0800

    清除A版本遗留的冗余文件（src/interface/、src/parameter/、src/structure/、src/widget/、resource/、.idea/ 等）

--- src/interface/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from interface.pilot_frame import PilotFrame
-from interface.prmgrp_frame import PrmgrpFrame
-from interface.robot_frame import RobotFrame
-from interface.scenario_frame import ScenarioFrame
-from interface.script_frame import ScriptFrame
-from interface.snmsg_frame import SnmsgFrame
--- src/structure/generic/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from structure.generic.rom import Rom
-from structure.generic.sequence import Sequence, SEQUENCE
-from structure.generic.text import Text
-from structure.generic.value import Value
